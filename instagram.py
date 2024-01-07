f8bbbaff56ab79f5f1114cf6c53de277fa0b3b81
Manish <manish.dinkar13@gmail.com>
2024-01-08 03:45:38 +0530

    unfollow

--- instagram.py
+++ instagram.py
@@ -4,5 +4,5 @@ bot = Bot()
 
 bot.login(username="python_somu1213", password="Demo31234")
 bot.follow('techview365')
-
+# bot.unfollow('sleepinthegardn')
 # bot.upload_photo("applicationdev.jpg", caption="This is a demo image")
