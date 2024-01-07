36d9a1ea81024ce8a8c5ca522c055c601b3fe417
Manish <manish.dinkar13@gmail.com>
2024-01-08 03:43:42 +0530

    first comment

---
+++ instagram.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+
+bot = Bot()
+
+bot.login(username="python_somu1213", password="Demo31234")
+bot.follow('techview365')
+
+# bot.upload_photo("applicationdev.jpg", caption="This is a demo image")
